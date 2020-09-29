201dc0669aa36bc3768b2c648604b2d6580d4016
Sebastian Werhausen <swerhausen@gmail.com>
2020-09-29 06:35:45 +0200

    limit the player position to not go too far off screen.

--- player.cpp
+++ player.cpp
@@ -21,6 +21,18 @@ namespace {
       return new_diff;
    }
 
+
+   /// <summary>Limits the actual player position to not go too far off screen. Otherwise it
+   /// would take a long time to go 'back' after mouse was somewhere else.</summary>
+   [[nodiscard]] constexpr auto get_limited_pos(const moo::FractionalPos& pos) -> moo::FractionalPos {
+      constexpr double x_limit = 0.1;
+      constexpr double y_limit = 0.5 * x_limit; // pixels are about half as high as they are wide
+      moo::FractionalPos new_pos = pos;
+      new_pos.x_fraction = std::clamp(new_pos.x_fraction, 0.0 - x_limit, 1.0 + x_limit);
+      new_pos.y_fraction = std::clamp(new_pos.y_fraction, 0.0 - y_limit, 1.0 + y_limit);
+      return new_pos;
+   }
+
 } // namespace {}
 
 
@@ -32,7 +44,7 @@ auto moo::Player::move_towards(
 ) -> void
 {
    const FractionalPos position_diff = get_sanitized_position_diff(target_pos - m_pos, rows, columns);
-   m_pos = m_pos + dt * m_speed * get_indep_normalized(position_diff);
+   m_pos = get_limited_pos(m_pos + dt * m_speed * get_indep_normalized(position_diff));
 
    m_bullet_wait = std::clamp(m_bullet_wait - dt, 0.0, 1.0);
 }
